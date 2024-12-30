97c022a5ef5a6902245154f62830d95e42d76f3a
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-12-31 00:28:33 +0700

    encode to base64 for password

--- UIT.CodeRelax.UseCases/DTOs/Requests/Authentication/LoginReq.cs
+++ UIT.CodeRelax.UseCases/DTOs/Requests/Authentication/LoginReq.cs
@@ -11,7 +11,26 @@ namespace UIT.CodeRelax.UseCases.DTOs.Requests.Authentication
     public class LoginReq
     {
         public string Email { get; set; }
-        public string Password { get; set; }
+
+        private string _password; // original
+
+        public string Password
+        {
+            get => _password;
+            set => _password = ConvertToBase64(value); 
+        }
+
+        // Encode password to base 64
+        private string ConvertToBase64(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return password;
+            }
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(passwordBytes);
+        }
     }
 
     public class LoginReqValidator : AbstractValidator<LoginReq>
--- UIT.CodeRelax.UseCases/DTOs/Requests/Authentication/SignUpReq.cs
+++ UIT.CodeRelax.UseCases/DTOs/Requests/Authentication/SignUpReq.cs
@@ -10,8 +10,25 @@ namespace UIT.CodeRelax.UseCases.DTOs.Requests.Authentication
     public class SignUpReq
     {
         public string Email { get; set; } = String.Empty;
-        public string Password { get; set; } = String.Empty;
+        private string _password;
+        public string Password { 
+            get => _password; 
+            set =>_password = ConvertToBase64(value); 
+        } 
         public string DisplayName { get; set; } = String.Empty;
+
+        // Encode password to base 64
+        private string ConvertToBase64(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return password;
+            }
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(passwordBytes);
+        }
+
     }
 
     public class SignUpReqValidator : AbstractValidator<SignUpReq>
@@ -19,7 +36,7 @@ namespace UIT.CodeRelax.UseCases.DTOs.Requests.Authentication
         public SignUpReqValidator()
         {
             RuleFor(x => x.Email).EmailAddress().WithMessage("Email is invalid Format");
-            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is invalid");
             RuleFor(x => x.DisplayName).NotEmpty();
         }
     }
